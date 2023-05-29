40c2fd5f1431bc514b9d6684df7a6d7bccfe33da
Kent Erik <kent.erik.hole@outlook.com>
2023-05-29 16:03:28 +0200

    addin defineConfig to vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import dns from 'dns'
-
-dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig({
 	server: {
+		host: 'localhost',
 		port: 5172,
 	},
 })
