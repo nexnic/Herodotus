440856d87575ba59175ff753da0b0f621cf3e6b0
Kent Erik <kent.erik.hole@outlook.com>
2023-05-30 16:26:16 +0200

    start on navigation

---
+++ src/js/templet/nav/nav.js
@@ -0,0 +1,65 @@
+/*
+export const templetNav = (UserIsLogin) => {
+    // Select Element 
+        // Top Navigation 
+            const top = document.querySelector('#nav--top')
+        // Bottom Navigation 
+            const bot = document.querySelector('#nav-bot')
+    // Creat Elelemt 
+        // Top Navigation 
+            // Left 
+                // container 
+                    const leftCon = document.createElement('div')
+                // Register Button 
+                    const regBtn = document.createElement('button')
+                // Profile Button
+                    const proBtn = document.createElement('button')
+                // Profile image 
+                    const proImg = document.createElement('button')
+                // profile Name 
+                    const proName = document.createElement('p')
+                //   
+            // Logo
+                // Container 
+                    const logoCon = document.createElement('div')
+                // Image 
+                    const logoImg = document.createElement('img')
+                // Logo text
+                    const logoText = document.createElement('h4')
+            // Right 
+                // Container
+                    const rightCon = document.createElement('div')
+                // Logout Button
+                    const logoutBtn = document.createElement('button')
+                // Login Button 
+                    const loginBtn = document.createElement('button')
+        // Button Navigation
+            // Container 
+                const buttonCon = document.createElement('div')
+            // Home Button
+                const homeBtn = document.createElement('button')
+            // Search Button 
+                const searchBtn = document.createElement('button')
+            // Add Button 
+                const addBtn = document.createElement('Button')
+            // Filter Button
+                const filterBtn = document.createElement('button')
+            // Users Button
+                const userBtn = document.createElement('button')       
+    
+
+
+    if(UserIsLogin){
+        console.log('User is login')
+        if(UserData.avatar) {
+            console.log('User has image')
+        } else {
+            console.log('User Has not image')
+        }
+
+    } else {
+        console.log('user not Login')
+    }
+
+}
+*/
