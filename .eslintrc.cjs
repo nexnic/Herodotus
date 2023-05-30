f56e4e613fd22861675bc77977a06d687be5eb3a
Kent Erik <kent.erik.hole@outlook.com>
2023-05-30 11:38:58 +0200

    adding cypress

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: ["eslint:recommended", "prettier"],
-  overrides: [
-    {
-      "files": ["**/*.cy.js"],
-      "env": { "cypress/globals": true },
-      "plugins": ["cypress"],
-      "extends": ["plugin:cypress/recommended"],
-      "rules": {
-        "cypress/no-unnecessary-waiting": "off",
-        "no-unused-vars": "off"
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-	"prettier/prettier": ["error"]
-  },
-  plugins: ["prettier"],
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
 };
---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+	/**
+ * 
+  e2e: {
+    setupNodeEvents(on, config) {
+      console.log(on)
+      console.log(config)
+    },
+  },
+   */
+});
