f4f15d806f188b13faf00dd571541c667238ace1
Kent Erik <kent.erik.hole@outlook.com>
2023-05-31 16:45:04 +0200

    coding api call for register user

---
+++ src/js/api/auth/register.js
@@ -0,0 +1,16 @@
+export async function registerUser(user) {
+	const response = await fetch(
+		"https://nf-api.onrender.com/api/v1/auction/auth/register",
+		{
+			method: "POST",
+			headers: {
+				"Content-type": "application/json; charset=UFT-8",
+			},
+			body: JSON.stringify(user),
+		}
+	);
+
+	if (response.ok) {
+		console.log(await response.json());
+	}
+}
