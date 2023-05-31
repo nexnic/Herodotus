611d290151baa48e3e8453211faec16b4445ca6e
Kent Erik <kent.erik.hole@outlook.com>
2023-05-31 16:58:21 +0200

    fix clean

--- src/js/storage/clean.js
+++ src/js/storage/clean.js
@@ -1,16 +1,3 @@
-/**
- *
- * @example
- * ```js
- * try
- * clean localstorage
- * pass
- * @returns true
- * fails
- * @returns fail
- * ```
- */
-
 export const cleanall = () => {
 	localStorage.clear();
 };
