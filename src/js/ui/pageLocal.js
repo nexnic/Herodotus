3a2236c08a4fb203f74d84d7d7e400e64dd61885
Kent Erik <kent.erik.hole@outlook.com>
2023-06-01 14:21:43 +0200

    adding home function

--- src/js/ui/pageLocal.js
+++ src/js/ui/pageLocal.js
@@ -1,13 +1,15 @@
 import { load } from "../storage/load";
+import { save } from "../storage/save";
+import { changeTitle } from "../tools/headtitle";
+import { templetNav } from "../templet/nav/nav";
+import { templetHeaderHome } from "../templet/home/header";
 
 export const PageLocal = () => {
-	const UserLogin = load("token");
-	console.log(UserLogin);
 	const onSite = load("page");
-
-	if (onSite == "home") {
-		console.log("home");
-	} else {
-		console.log("missing data");
+	templetNav();
+	if (!onSite || onSite == "home") {
+		save("page", "home");
+		changeTitle("Herodotus | home");
+		templetHeaderHome();
 	}
 };
