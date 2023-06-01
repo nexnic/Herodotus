d73ad46542566526cd594fedc620ccddba212507
Kent Erik <kent.erik.hole@outlook.com>
2023-06-01 14:44:39 +0200

    adding apiCall to home

--- src/js/ui/home/pageConfig.js
+++ src/js/ui/home/pageConfig.js
@@ -1,9 +1,11 @@
 import { save } from "../../storage/save";
 import { changeTitle } from "../../tools/headtitle";
 import { templetHeaderHome } from "../../templet/home/header";
+import { GetAllListings } from "../../api/listings/allListings";
 
 export const HomeConfig = () => {
 	save("page", "home");
 	changeTitle("Herodotus | home");
 	templetHeaderHome();
+	GetAllListings();
 };
