f7ed1b4a771f2614725568b606a7c96c172b1d87
Kent Erik <kent.erik.hole@outlook.com>
2023-06-01 14:39:39 +0200

    add home to the home site config

--- src/js/ui/pageLocal.js
+++ src/js/ui/pageLocal.js
@@ -1,15 +1,12 @@
 import { load } from "../storage/load";
-import { save } from "../storage/save";
-import { changeTitle } from "../tools/headtitle";
+import home from "./home/index";
+
 import { templetNav } from "../templet/nav/nav";
-import { templetHeaderHome } from "../templet/home/header";
 
 export const PageLocal = () => {
 	const onSite = load("page");
 	templetNav();
 	if (!onSite || onSite == "home") {
-		save("page", "home");
-		changeTitle("Herodotus | home");
-		templetHeaderHome();
+		home();
 	}
 };
