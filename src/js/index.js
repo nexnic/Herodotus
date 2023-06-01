eaf9026f95f0dacb1f3811d560bc80a744735071
Kent Erik <kent.erik.hole@outlook.com>
2023-06-01 14:43:58 +0200

    remove api call from index

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 import listeners from "./listeners/index";
-import { GetAllListings } from "./api/listings/allListings";
-
 import ui from "./ui/index.js";
 
 ui();
 listeners();
-
-GetAllListings();
