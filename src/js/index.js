da63169585d56eae64ff9306a3a2881d08a05cac
Kent Erik <kent.erik.hole@outlook.com>
2023-06-01 13:30:14 +0200

    adding home

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import { templetNav } from "./templet/nav/nav";
+import { templetHeaderHome } from "./templet/home/header";
 import listeners from "./listeners/index";
 
 templetNav();
-
+templetHeaderHome();
 import ui from "./ui/index.js";
 
 ui();
