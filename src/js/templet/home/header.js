bb3231d152f8fe8a8e1b269c667a88a00d25c64d
Kent Erik <kent.erik.hole@outlook.com>
2023-06-01 13:29:30 +0200

    coding header

--- src/js/templet/home/header.js
+++ src/js/templet/home/header.js
@@ -9,6 +9,14 @@ export const templetHeaderHome = () => {
 	const logoBox = document.createElement("div");
 	// Text Content
 	const textBox = document.createElement("div");
+	// Logo Image
+	const imgLogo = document.createElement("img");
+	// Headline
+	const h1 = document.createElement("h1");
+	const headMain = document.createElement("span");
+	const headSub = document.createElement("span");
+	const br = document.createElement("br");
+
 	// Register Button
 	const regBtn = document.createElement("button");
 	// Set Attribut
@@ -21,11 +29,32 @@ export const templetHeaderHome = () => {
 	// header
 	header.classList.add("d-flex", "justify-content-center");
 	// Container
-	container.classList.add("d-flex", "justify-content-center", "w-75");
+	container.classList.add(
+		"d-flex",
+		"flex-column",
+		"justify-content-center",
+		"w-75",
+		"bg-primary",
+		"p-3"
+	);
+	textBox.classList.add("text-center", "pt-2");
+	// Register button
+	regBtn.classList.add("btn");
+	// Set Text
+	headMain.innerText = "Herodotus";
+	headSub.innerText =
+		"Welcome to our auction website, where you can buy and sell a wide range of products and services through a transparent and competitive bidding process.";
+	regBtn.innerText = "register";
 
 	// AppendChild
 	header.appendChild(container);
 	container.appendChild(logoBox);
 	container.appendChild(textBox);
+	textBox.appendChild(imgLogo);
+	textBox.appendChild(h1);
+	h1.appendChild(headMain);
+	h1.appendChild(br);
+	h1.appendChild(headSub);
+
 	container.appendChild(regBtn);
 };
