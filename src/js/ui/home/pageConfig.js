974511cd3a9a009438736095c44699219642c9dc
Kent Erik <kent.erik.hole@outlook.com>
2023-06-09 11:45:28 +0200

    adding ui

--- src/js/ui/home/pageConfig.js
+++ src/js/ui/home/pageConfig.js
@@ -4,6 +4,15 @@ import { templetHeaderHome } from "../../templet/home/header";
 import { GetAllListings } from "../../api/listings/allListings";
 
 export const HomeConfig = () => {
+	const selectmain = document.querySelector("main");
+	selectmain.classList.add(
+		"d-flex",
+		"justify-content-center",
+		"align-items-center",
+		"flex-column",
+		"gap-3",
+		"pt-3"
+	);
 	save("page", "home");
 	changeTitle("Herodotus | home");
 	templetHeaderHome();
