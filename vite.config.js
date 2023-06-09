af15e1cd17e8c09d6f17ebfe50ed17dafde994d8
Kent Erik <kent.erik.hole@outlook.com>
2023-06-09 11:46:02 +0200

    adding config for vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	server: {
-		host: 'localhost',
+		host: true,
 		port: 5172,
 	},
-})
+});
