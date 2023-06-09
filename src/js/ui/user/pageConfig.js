d85c1b4da0a4005cb8a871aca0519699710fee1d
Kent Erik <kent.erik.hole@outlook.com>
2023-06-09 17:51:47 +0200

    testing bugg

--- src/js/ui/user/pageConfig.js
+++ src/js/ui/user/pageConfig.js
@@ -1,6 +1,11 @@
-/*
 import { save } from "../../storage/save";
+import { load } from "../../storage/load";
 import { changeTitle } from "../../tools/headtitle";
-*/
 
-export const userConfig = () => {};
+export const userConfig = () => {
+	const user = load("userData");
+	save("page", "userprofile");
+	changeTitle(`Herodotus |Profile | ${user.name} `);
+
+	document.querySelector("main").innerHTML = "";
+};
